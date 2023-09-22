4d18344aeebeeaa0887911875aa40fc3392ab385
sumit7200 <rajsumit7200@gmail.com>
2023-09-22 18:57:37 +0530

    change in css

--- Xrpl.js
+++ Xrpl.js
@@ -1,31 +1,29 @@
-// Initialize RippleAPI (Replace with your credentials)
+
 const RippleAPI = require('ripple-lib').RippleAPI;
 const api = new RippleAPI({ server: 'wss://s1.ripple.com' });
 
-// Connect to the XRPL server
+
 api.connect().then(() => {
     console.log('Connected to XRPL');
 }).catch((error) => {
     console.error('Error connecting to XRPL:', error);
 });
 
-// Get account balance and display it
 const accountAddress = 'YOUR_ACCOUNT_ADDRESS';
 api.getAccountInfo(accountAddress).then((info) => {
-    const balance = info.xrpBalance / 1000000; // Convert from drops to XRP
+    const balance = info.xrpBalance / 1000000;
     document.getElementById('balance').textContent = balance.toFixed(6);
 }).catch((error) => {
     console.error('Error fetching account balance:', error);
 });
 
-// Handle XRP send form submission
 const sendForm = document.getElementById('send-form');
 sendForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const recipient = document.getElementById('recipient').value;
     const amount = document.getElementById('amount').value;
 
-    // Create and submit a payment transaction (Replace with your logic)
+
     const payment = {
         source: {
             address: accountAddress,
@@ -44,15 +42,14 @@ sendForm.addEventListener('submit', (e) => {
     };
 
     api.preparePayment(accountAddress, payment).then((prepared) => {
-        // Sign the transaction (Replace with your signing logic)
-        // Send the transaction (Replace with your sending logic)
+
         console.log('Transaction prepared:', prepared.txJSON);
     }).catch((error) => {
         console.error('Error preparing payment:', error);
     });
 });
 
-// Get transaction history and display it
+
 const transactionHistory = document.getElementById('transaction-history');
 api.getTransactions(accountAddress).then((transactions) => {
     transactions.forEach((tx) => {
